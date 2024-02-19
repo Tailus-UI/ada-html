68a5459cff50662eba616b8403c10093d75c692b
Méschac Irung <47919550+Meschacirung@users.noreply.github.com>
2024-02-19 09:51:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+<h1>Tailwind 3.1 - Vite boilerplate</h1>   
+
+A tailwindcss v3 - Vite project starter
+
+<h2>Tools</h2> 
+
+- Tailwindcss v3 : UI
+- Vite 2.9
+- Vanilla
+- Prettier
+- Postcss 
+
+## Build Setup
+
+```bash
+# install dependencies
+$ npm install
+# serve with hot reload at localhost:3000
+$ npm run dev
+# build for production and launch server
+$ npm run build
+# preview production
+$ npm run preview
+```
+
+### `assets`
+
+The assets directory contains your uncompiled assets such as Stylus or Sass files, images, or fonts.
+
+### `src`
+
+The src directory contains your app ui (html files) and your JavaScript.
---
+++ main.js
@@ -0,0 +1,35 @@
+import "./assets/css/tailwind.css";
+import "./src/js/navbar.js"
+
+let switchers = document.querySelectorAll(".switcher");
+if (
+  localStorage.getItem("color-theme") === "dark" ||
+  (!("color-theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
+}
+
+switchers.forEach((switcher) => {
+  switcher.addEventListener("click", function () {
+    if (localStorage.getItem("color-theme")) {
+      if (localStorage.getItem("color-theme") === "light") {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("color-theme", "dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("color-theme", "light");
+      }
+    } else {
+      if (document.documentElement.classList.contains("dark")) {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("color-theme", "light");
+      } else {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("color-theme", "dark");
+      }
+    }
+  });
+});
