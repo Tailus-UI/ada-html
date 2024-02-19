5a8f3ef67154d5f1b3f9c80a32bec15eb66600f3
Meschac Irung <meschacirung@gmail.com>
2024-02-19 17:27:58 +0200

    Add custom font files and update tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 import themer from "@tailus/themer";
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
     content: ["./*.html", "./*.js"],
@@ -18,14 +19,19 @@ module.exports = {
             white: colors.white,
             black: colors.black,
             transparent: colors.transparent,
-        }),
+      }),
+      fontFamily: {
+        sans: ['Geist', 'Inter', ...defaultTheme.fontFamily.sans],
+        mono : ['GeistMono', 'fira-code', ...defaultTheme.fontFamily.mono],
+      },
         keyframes: {
             loop: {
                 to: {
                     "offset-distance": "100%",
                 },
             },
-        }
+        },
+        
     },
     plugins: [
         themer({
