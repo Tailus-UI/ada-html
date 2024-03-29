09330e6841e34c45f1ef3c1d2952646353820ba0
Meschac Irung <meschacirung@gmail.com>
2024-03-29 13:37:30 +0200

    update [themer] : integrate default themer palettes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,19 +7,6 @@ module.exports = {
     darkMode: "media",
     safelist: ["isToggled"],
     theme: {
-        colors: ({ colors }) => ({
-            primary: colors.blue,
-            secondary: colors.lime,
-            accent: colors.pink,
-            success: colors.lime,
-            danger: colors.red,
-            warning: colors.yellow,
-            info: colors.blue,
-            gray: colors.zinc,
-            white: colors.white,
-            black: colors.black,
-            transparent: colors.transparent,
-      }),
       fontFamily: {
         sans: ['Geist', 'Inter', ...defaultTheme.fontFamily.sans],
         mono : ['GeistMono', 'fira-code', ...defaultTheme.fontFamily.mono],
@@ -35,8 +22,11 @@ module.exports = {
     },
     plugins: [
         themer({
+            palette: {
+                extend : "nature"
+            },
             radius: "smoothest",
-            background: "lighter",
+            background: "light",
             border: "light",
             padding:"large",
             components: {
