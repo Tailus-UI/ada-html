0a75ca7e1072bd9b0f2bb34357c9ba74a24f7333
Ankur Agarwal <signup.ankur@gmail.com>
2024-09-19 21:50:30 +0530

    updated landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 import themer from "@tailus/themer";
 const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require('tailwindcss/colors')
+
 
 module.exports = {
     content: ["./*.html", "./*.js"],
     darkMode: "media",
     safelist: ["isToggled"],
     theme: {
+        extend: {
+            colors: {
+                'primary': {
+        '50': '#eff1fe',
+        '100': '#e1e7fe',
+        '200': '#c9d1fc',
+        '300': '#a8b3f9',
+        '400': '#868cf3',
+        '500': '#6a68ec',
+        '600': '#5a4cdf',
+        '700': '#4d3ec4',
+        '800': '#3f349f',
+        '900': '#37317e',
+        '950': '#1e1a42',
+    },
+      
+            },
+        },
+
       fontFamily: {
         sans: ['Geist', 'Inter', ...defaultTheme.fontFamily.sans],
         mono : ['GeistMono', 'fira-code', ...defaultTheme.fontFamily.mono],
